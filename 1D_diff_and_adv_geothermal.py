a7dc5a78c9f3f8368cda09522434bdda49fdcafe
lachmccallum <l_mccallum@coloradocollege.edu>
2024-12-17 21:51:12 -0700

    removed readmetext

--- 1D_diff_and_adv_geothermal.py
+++ 1D_diff_and_adv_geothermal.py
@@ -1,17 +1,3 @@
-'''
-This model iterates on my previous Week 2 Project, adding the advection portion of
-a geothermal system and importing data from the literature. As water is reinjected
-into a geothermal reservoir, how does the temperature gradient change? I found data
-from the literature to import into this model to investigate two variables:
-reinjection rate and reinjection temperature. 
-
-Data from Rivera Diaz, Alexandre, Eylem Kaya, and Sadiq J. Zarrouk. “Reinjection in 
-Geothermal Fields − A Worldwide Review Update.” Renewable and Sustainable Energy Reviews 
-53 (January 1, 2016): 105–62. https://doi.org/10.1016/j.rser.2015.07.151
-
-'''
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -24,18 +10,6 @@ data = pd.read_csv('geothermal_well_data.csv')
 reinjection_rates = np.array(data.iloc[1:, 4]) ## reinjection rate of geothermal fluid in tonne/hr
 temperatures = np.array(data.iloc[1:, 5]) ## reinjection temperature of geothermal fluid in *C
 reservoirs = data.iloc[1:, 2].values ## type of reservoir: hot water, 2-phase low enthalpy, 2-phase medium enthalpy, 2-phase high enthalpy
-
-
-'''
-This model utilizes a function that will change the value for reinjection rate,
-pulled from the above .csv, and later on add in injection temperature. Since the 
-review I pulled the data from did not report thermal conductivity, depth of injection,
-thermal gradient of the reservoir, or density of geothermal fluid, I have opted to keep
-those constant for all of the model. Future work could focus on discerning these parameters
-and updating the model with them.
-That being said, small tweaks in this model could result in instability - for example,
-changing the dt or dz steps. If this occurs, then reset the model to the preset options.
-'''
 
 def geothermal_model(reinjection_rate, temperature):
     ## Convert reinjection rate to velocity -- Q = vA or v = Q/A
